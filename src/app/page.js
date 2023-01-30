1f7b98badb0a2938ad3150279fc57f8cf8794834
Rahilsiddique <siddiquerahil19@gmail.com>
2023-01-30 15:00:08 +0530

    pocketbase
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,14 @@
 
 export default function Home() {
   return (
-    <div className='text-3xl font-bold underline'>hello</div>
+    <div>
+      <button className="btn">Button</button>
+      <div className="form-control">
+        <label className="label cursor-pointer">
+          <span className="label-text">Remember me</span>
+          <input type="checkbox" className="toggle" checked />
+        </label>
+      </div>
+    </div>
   )
 }
